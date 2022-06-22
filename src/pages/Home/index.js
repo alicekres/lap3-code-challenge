3ebb950ce53e9142a1fc5fc0bf03c443eefc1a2b
GMillerMc <gmiller04@yahoo.com>
2022-06-22 10:45:44 +0100

    change home layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,16 +4,14 @@ import './style.css';
 
 export const Home = () => {
     return(
+        
         <div className="App">
-            <header className="App-header">
-            {error
-                ? <h1>Sorry, we could not find a {search} Repo</h1>
-                : <div>
-                <h3> {statusMessage ? statusMessage : true } </h3>
-                <ul> {renderedRepos} </ul>
+        
+                <ul> Repos </ul>
                 </div>
-            }
-            </header>
-        </div>
+            
+            
+        
+       
     )
 }
