b8b8d0c66155f9166a45d23de56987abfb6f9897
alicekres <alice.kreslins@gmail.com>
2022-06-22 13:14:06 +0100

    add styling on navbar

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { FaPlusCircle } from 'react-icons/fa';
+import { IoMdArrowDropdownCircle } from 'react-icons/io';
 
 import styles from './index.module.css';
 import githubPic from '../../assets/images/github.png';
+import bell from '../../assets/images/notification.png';
 
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-dark">
-            <div class="container-fluid">
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <div className="container-fluid">
                 <a href="/">
                     <img
                         src={githubPic}
@@ -15,7 +18,7 @@ const Header = () => {
                         height={40}
                     />
                 </a>
-                <form class="d-flex" role="search" className={styles.form}>
+                <form className={`d-flex ${styles.form}`} role="search">
                     <label></label>
                     <input
                         className={`form-control mx-4 ${styles.input}`}
@@ -25,7 +28,7 @@ const Header = () => {
                     />
                 </form>
                 <button
-                    class="navbar-toggler"
+                    className="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent"
@@ -33,29 +36,29 @@ const Header = () => {
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div
-                    class="collapse navbar-collapse"
+                    className="collapse navbar-collapse"
                     id="navbarSupportedContent"
                 >
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
                             <a className={`nav-link ${styles.a}`} href="#">
                                 Pull Requests
                             </a>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <a className={`nav-link ${styles.a}`} href="#">
                                 Issues
                             </a>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <a className={`nav-link ${styles.a}`} href="#">
                                 Marketplace
                             </a>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <a className={`nav-link ${styles.a}`} href="#">
                                 Explore
                             </a>
@@ -63,6 +66,15 @@ const Header = () => {
                     </ul>
                 </div>
             </div>
+            <img
+                className={styles.image}
+                src={bell}
+                alt="notification bell icon"
+                width={23}
+                height={25}
+            />
+            <FaPlusCircle size={25} color="white" />
+            <IoMdArrowDropdownCircle color="white" size={30} />
         </nav>
     );
 };
