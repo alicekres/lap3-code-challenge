3db17f105681751e24e0eb6a83c1d1046bdad333
GMillerMc <gmiller04@yahoo.com>
2022-06-22 10:31:58 +0100

    add search render to homepage

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './index.module.css';
+// import styles from './index.module.css';
 
 const Header = () => {
     return <nav></nav>;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,16 @@ import './style.css';
 
 export const Home = () => {
     return(
-        <>
-        <div>
-        <h1>Github</h1>
-        
+        <div className="App">
+            <header className="App-header">
+            {error
+                ? <h1>Sorry, we could not find a {search} Repo</h1>
+                : <div>
+                <h3> {statusMessage ? statusMessage : true } </h3>
+                <ul> {renderedRepos} </ul>
+                </div>
+            }
+            </header>
         </div>
-        </>
     )
 }
